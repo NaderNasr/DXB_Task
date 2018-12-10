eacd6ea323d0977e04c2a3aed4cc4e6db8b3987b
Nader Nasr <nadernasr7@gmail.com>
2018-12-11 01:42:05 +0200

    rotating cube!

--- js/app.js
+++ js/app.js
@@ -1,44 +1,86 @@
+// these need to be accessed inside more than one function so we'll declare them first
 let container;
 let camera;
 let renderer;
 let scene;
 let mesh;
 
-// A function to initialize or start the scene
-function init(){
+function init() {
 
-  // Container element that will adopt our scene
-  const container = document.querySelector( '#container' );
+  // Get a reference to the container element that will hold our scene
+  container = document.querySelector( '#container' );
 
   // create a Scene
-  const scene = new THREE.Scene();
-  scene.background = new THREE.Color( 'skyblue' );
+  scene = new THREE.Scene();
+  // Set the background color
+  scene.background = new THREE.Color( 0x8FBCD4 );
 
-  // Create a Camera
-  const fov = 35; // Camera Field of View
+  // set up the options for a perspective camera
+  const fov = 35; // fov = Field Of View
   const aspect = container.clientWidth / container.clientHeight;
 
   const near = 0.1;
   const far = 100;
 
-  const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+  camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+
+  // every object is initially created at ( 0, 0, 0 )
+  // we'll move the camera back a bit so that we can view the scene
   camera.position.set( 0, 0, 10 );
 
+  // create a geometry
   const geometry = new THREE.BoxBufferGeometry( 2, 2, 2 );
 
-  const material = new THREE.MeshBasicMaterial();
 
-  const mesh = new THREE.Mesh( geometry, material );
+  // create a purple Standard material
+  const material = new THREE.MeshStandardMaterial( { color: 0x800080 } );
+
+  // create a Mesh containing the geometry and material
+  mesh = new THREE.Mesh( geometry, material );
+
+  // add the mesh to the scene object
   scene.add( mesh );
 
-  const renderer = new THREE.WebGLRenderer();
+  // Create a directional light
+ const light = new THREE.DirectionalLight( 0xffffff, 5.0 );
 
+ // move the light back and up a bit
+ light.position.set( 0, 3, 3 );
+
+ // remember to add the light to the scene
+ scene.add( light );
+
+  // create a WebGLRenderer and set its width and height
+  renderer = new THREE.WebGLRenderer();
   renderer.setSize( container.clientWidth, container.clientHeight );
+
   renderer.setPixelRatio( window.devicePixelRatio );
+
+  // add the automatically created <canvas> element to the page
   container.appendChild( renderer.domElement );
+
 }
 
-// function to being animation of the 3D object 
-function animate(){
+function animate() {
+
+  // call animate recursively
+  requestAnimationFrame( animate );
+
+  // increase the mesh's rotation each frame
+  mesh.rotation.z += 0.01;
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.01;
+  // console.log(requestAnimationFrame)
+
+  // render, or 'create a still image', of the scene
+  // this will create one still image / frame each time the animate
+  // function calls itself
   renderer.render( scene, camera );
+
 }
+
+// call the init function to set everything up
+init();
+
+// then call the animate function to render the scene
+animate();
